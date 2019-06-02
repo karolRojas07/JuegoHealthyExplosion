724ac5931a742673ff96361b35a17a782a74a8db
Karol Rojas <>
2019-06-01 22:17:53 -0500

    Documentación de la clase Game

--- src/Controlador/Game.java
+++ src/Controlador/Game.java
@@ -6,111 +6,87 @@
 package Controlador;
 
 import Containers.Container;
-import ElementsScenarios.Antibiotic;
-import ElementsScenarios.Bomb;
-import ElementsScenarios.Box;
-import ElementsScenarios.Wall;
-import ElementsScenarios.Wood;
 import Readers.DataGameReader;
-import Readers.ReaderStage;
-import Scenarios.Estomach;
 import Scenarios.Stage;
 import Spriters.Sprite;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 
 /**
- *
- * @author usuario
+ * The controller of the game
+ * @author María y Karol
  */
 public class Game extends Sprite implements Container{
 
+    /**
+     * It is responsible of read the data information about a stage
+     */
     private DataGameReader readerStage ;
+    
+    /**
+     * It means the level of the games
+     */
     private Stage stage;
    
-
+    /**
+     * Create a new game
+     * @param x
+     * @param y
+     * @param width
+     * @param height 
+     */
     public Game(int x, int y, int width, int height) {
         super(x, y, width, height);
       
     }
     
+    /**
+     * Define the reader of the stage data information
+     * @param readerStage 
+     */
     public void setReaderStage(DataGameReader readerStage) {
         this.readerStage = readerStage;
     }
     
     /**
-     * guarda el escenario
+     * Define the stage
      * @param stage 
      */
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-   
+    public void setStage(Stage stage) { this.stage = stage;  }
     
     /**
-     * 
+     * Draw the stage
      * @param g 
      */
     @Override
     public void draw(Graphics g) {
-     
-       
         g.setColor(Color.DARK_GRAY);
         g.fillRect(super.getX(), super.getY(), super.getWidth(), super.getHeight());
-        
         stage.drawBox(g);
-     
-        
     }
 
-    
     /**
-     * 
+     * Get the event when a key is pressed and tell to the stage
      * @param evt 
      */
-     public void keyPressed(KeyEvent evt) 
-    {
-        
-        
-         stage.keyPressed(evt);
-       
-        
-    }
+     public void keyPressed(KeyEvent evt) { stage.keyPressed(evt); }
       
    
      /**
-      * 
+      * Uptdate the game in the view
       */
     @Override
-    public void refresh() {
-        super.getContainer().refresh();
-        
-        
-    }
+    public void refresh() { super.getContainer().refresh();}
 
     /**
-     * 
-     * @return 
+     * Get the measurements of the game
+     * @return Rectangle object that is used in the graph part
      */
     @Override
     public Rectangle getBoundaries() {
-        
-        return new Rectangle(getX(), getY(), getWidth(), getHeight());
+        return new Rectangle(super.getX(), super.getY(), super.getWidth(), super.getHeight());
     }
-
-    public void mousePressed(MouseEvent evt) {
-    }
-    
-    
-    
-
-    
+  
 }
